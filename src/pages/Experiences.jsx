4fa6ce3098924aeed120f4e0685963ef7055b324
s4mi-sb <samuelsolomon444@gmail.com>
2024-10-06 17:06:55 -0500

    added arconic

--- src/pages/Experiences.jsx
+++ src/pages/Experiences.jsx
@@ -1,4 +1,5 @@
 import profile from '../assets/innovate_120.jpeg'
+import arconicProfile from '../assets/arconic.jpg'
 import {useEffect, useRef} from 'react'
 import { motion,useInView,useAnimation} from "framer-motion"
 
@@ -30,6 +31,53 @@ export default function Experiences() {
       
       >
         <h1 className='text-3xl text-blue-200 text-center font-semibold underline underline-offset-8 mb-7'>Experience</h1>
+
+        {/* Arconic */}
+        <div className='flex flex-row justify-between bg-blue-400 p-2 rounded-md items-center mb-5'>
+            <div>
+                <h1 className='font-semibold text-xl'>Electrical Engineering Intern</h1>
+                <p>May 2025 - August 2025</p>
+                <motion.p
+                className='InnovateHeading'>
+                  
+                    <motion.a whileHover={{fontWeight: 'bold'}} href="https://www.arconic.com/" target="_blank" rel="noopener noreferrer"> Arconic</motion.a>
+                </motion.p>
+                
+            </div>
+
+            <div>
+                <img className='h-full object-cover size-20 border border-slate-400 rounded-lg' src={arconicProfile} alt="" />
+            </div>
+        </div>
+        <motion.ul 
+          animate = {modify}
+          initial = "hidden"
+          variants={
+            {
+              hidden: {opacity: 0, y: -100},
+              visible: {opacity: 1, y: 0},
+            }
+          }
+          transition={
+            {delay: 0.3}}
+        
+        className='gap-2 flex flex-col p-2 text-white mb-10'>
+                <motion.li 
+                animate = {modify}
+                initial = "hidden"
+                variants={
+                  {
+                    hidden: {opacity: 0, y: 100},
+                    visible: {opacity: 1, y: 0},
+                  }
+                }
+                transition={
+                  {delay: 0.5}}
+                
+                >➤ Incoming Summer 2025 Electrical Engineering Intern.</motion.li>
+        </motion.ul>    
+
+        {/* Innovate 120 */}
         <div className='flex flex-row justify-between bg-white p-2 rounded-md items-center mb-5'>
             <div>
                 <h1 className='font-semibold text-xl'>UI/UX Intern</h1>
@@ -55,7 +103,7 @@ export default function Experiences() {
             }
           }
           transition={
-            {delay: 0.3}}
+            {delay: 0.4}}
         
         className='gap-2 flex flex-col p-2 text-white'>
                 <motion.li 
@@ -68,7 +116,7 @@ export default function Experiences() {
                   }
                 }
                 transition={
-                  {delay: 0.5}}
+                  {delay: 0.6}}
                 
                 >➤ Worked with <motion.a whileHover={{fontWeight: 'bold'}} href="https://www.gaf.com" target="_blank" className='font-bold text-blue-400' rel="noopener noreferrer">GAF</motion.a> to improve roofers safety by using AI.</motion.li>
                 <motion.li 
@@ -81,7 +129,7 @@ export default function Experiences() {
                   }
                 }
                 transition={
-                  {delay: 0.6}}
+                  {delay: 0.7}}
                 
                 
                 
@@ -96,7 +144,7 @@ export default function Experiences() {
                   }
                 }
                 transition={
-                  {delay: 0.7}}
+                  {delay: 0.8}}
                 
                 >➤ Utilized Figma and Adobe to build a professional mobile app and wearable solution mock-up.</motion.li>
                 <motion.li 
@@ -109,7 +157,7 @@ export default function Experiences() {
                   }
                 }
                 transition={
-                  {delay: 0.8}}
+                  {delay: 0.9}}
                 
                 >➤ Gained confidence in strategic thinking, collaborative work, and interpersonal communication skills.</motion.li>
                 <motion.li 
@@ -122,7 +170,7 @@ export default function Experiences() {
                   }
                 }
                 transition={
-                  {delay: 0.9}}
+                  {delay: 1.0}}
                 
                 >➤ Demonstrated collaborative effort and innovative thinking in tackling industry-specific safety challenges.</motion.li>
         </motion.ul>
